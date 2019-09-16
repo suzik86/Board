5ac8229f750d36024b973264614cdce8f3ce19fc
Irina Shishkova <i.suzanovich@mail.ru>
2019-09-16 19:13:16 +0300

    init: add project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { BoardComponent } from './components/board/board.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardItemComponent } from './components/card-item/card-item.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    ToolbarComponent,
+    BoardComponent,
+    CardListComponent,
+    CardItemComponent,
   ],
   imports: [
     BrowserModule
